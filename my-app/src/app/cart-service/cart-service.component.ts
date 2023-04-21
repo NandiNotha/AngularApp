f4b23e338ef03560b7db1133d262b14e15bd79c5
https://github.com/github-NandiNotha <nandieh99@gmail.com>
2023-04-21 13:03:43 -0700

    changes

--- my-app/src/app/cart-service/cart-service.component.ts
+++ my-app/src/app/cart-service/cart-service.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsComponent } from '../products/products.component';
 // import { Product } from './products';
 
 @Component({
@@ -7,19 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-service.component.scss']
 })
 export class CartServiceComponent {
-//   items: Product[] = [];
-// /* . . . */
+  items: ProductsComponent[] = [];
+/* . . . */
 
-//   addToCart(product: Product) {
-//     this.items.push(product);
-//   }
+  addToCart(product: ProductsComponent) {
+    this.items.push(product);
+  }
 
-//   getItems() {
-//     return this.items;
-//   }
+  getItems() {
+    return this.items;
+  }
 
-//   clearCart() {
-//     this.items = [];
-//     return this.items;
-//   }
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
+
+// export class ProductsComponent implements OnInit {
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private cartService: CartService
+//   ) { }
+//   ngOnInit(): void {
+//     throw new Error('Method not implemented.');
+//   }
+// }
+
+// export class ProductsComponent implements OnInit {
+//   addToCart(product: Product) {
+//     this.cartService.addToCart(product);
+//     window.alert('Your product has been added to the cart!');
+//   }
+// }
+
--- my-app/src/app/products/products.component.ts
+++ my-app/src/app/products/products.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
+export interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  cart: Product[] = [];
+
+  addToCart() {
+    let product: Product = {
+      name: "Product Name",
+      price: 9.99,
+      quantity: 1
+    };
+
+    this.cart.push(product);
+  }
 
 }
